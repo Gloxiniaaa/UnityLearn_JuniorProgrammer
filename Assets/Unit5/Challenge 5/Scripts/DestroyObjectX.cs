01490820ac3f67b1848e3b8b88834a9cbfd5dd37
Gloxiniaaa <hnm142758@gmail.com>
2026-06-15 18:29:29 +0700

    add challenge 5 source files

---
+++ Assets/Unit5/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Unit5.Challenge5.Scripts
+{
+    public class DestroyObjectX : MonoBehaviour
+    {
+        void Start()
+        {
+            Destroy(gameObject, 2); // destroy particle after 2 seconds
+        }
+
+    }
+
+}
